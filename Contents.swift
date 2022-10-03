2eae6aab1f7dba70b274d33ff30bad5d0a37ae9a
bchaker <36565291+bchaker@users.noreply.github.com>
2022-10-04 08:53:24 +1100

    Add files via upload

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+import Cocoa
+
+let celcius = 40.0
+
+let farhenheit = celcius * 9 / 5 + 32
+
+print("The tempreature in celcius is \(celcius)° | The temprature in Farhenheit is \(farhenheit)°")
+
+let numbers = [4, 8, 15, 16, 23, 42]
