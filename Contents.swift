26f32804ff8952b79e32b2791bbcc7f3dae916c1
bchaker <36565291+bchaker@users.noreply.github.com>
2022-10-04 08:53:43 +1100

    Update Contents.swift

--- Contents.swift
+++ Contents.swift
@@ -5,5 +5,3 @@ let celcius = 40.0
 let farhenheit = celcius * 9 / 5 + 32
 
 print("The tempreature in celcius is \(celcius)° | The temprature in Farhenheit is \(farhenheit)°")
-
-let numbers = [4, 8, 15, 16, 23, 42]
