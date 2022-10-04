bb21616564e6b87a0ec73e6629c4f19ddf487037
bchaker <36565291+bchaker@users.noreply.github.com>
2022-10-04 19:37:42 +1100

    Create Checkpoint2.swift

---
+++ Checkpoint2.swift
@@ -0,0 +1,10 @@
+import Cocoa
+
+var iceCreams = ["Ben & Jerry", "Milo", "Magnum", "Soft-serve", "Milo"]
+
+let iceCreamSet = Set(iceCreams)
+
+print(iceCreams.count)
+print(iceCreamSet.count)
+
+print(iceCreamSet)
